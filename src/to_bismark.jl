2aafe9813ebca20a7f65621f4afb16536621e8ea
Christos Palaiokostas <chpalaiok@gmail.com>
2025-08-07 14:57:54 +0200

    Restructuring

--- src/to_bismark.jl
+++ src/to_bismark.jl
@@ -6,7 +6,7 @@ create bismark type of files. Expects a dataframe with normalized feature counts
 """
 function to_bismark(sample::AbstractString,max_values::AbstractVector, df::DataFrame)
     rel_meth = max_values .- df[!,sample]
-    df = hcat(df[!,Cols(:Chr,:Start,:Strand)],rel_meth,df[!,sample],makeunique=true)
+    df = hcat(df[!,Cols(:Chrom,:Start,:Strand)],rel_meth,df[!,sample],makeunique=true)
     df.pseudo_pattern_1 .= "CG"
     df.pseudo_pattern_2 .= "CG"
     CSV.write("bismark_files/bismark_format_$sample.txt", df; delim="\t", writeheader=false)
